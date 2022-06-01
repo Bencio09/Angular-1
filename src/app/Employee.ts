32adb6de9aaab4890cc6a1c846e9028ffbc380a7
lai2-01 <lai2-01@itismeucci.com>
2022-06-01 13:36:00 +0200

    commiy

---
+++ src/app/Employee.ts
@@ -0,0 +1,9 @@
+export class Employee{
+    constructor(public id?: number, public firstName?: string, public lastName?: string) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+
+
